3bc6915e1d45b61013e14a2c0880f20b2ecfca5c
mBrownColoma <browncoloma@gmail.com>
2022-12-28 10:12:08 -0300

    "json con rut limpio"

--- propuesta2.php
+++ propuesta2.php
@@ -33,19 +33,47 @@ $j = json_encode($result["count"]);
 //cambiar j a int
 
 // var_dump($data);
-for ($i = 0; $i < 25; ++$i){
-	if($result["inbox"][$i]["subject"] == "Nuevo Contacto Enlace - Puerto Horizonte"){
+for ($i = 0; $i < 10; ++$i){
+	if($result["inbox"][$i]["subject"] == "Nuevo Contacto Enlace - Sendero del Monte"){
 		echo '<pre>';
         echo($result["inbox"][$i]["subject"]);
 		echo '<pre>';
         echo($result["inbox"][$i]["message"]);
+		
+		$content=$result["inbox"][$i]["message"];
+		echo ("---------------------");
+		$str_open  = "FijoFicha Completa";
+		$str_close = "Ver Ficha";
+		$pos_open  = strpos($content, $str_open);
+		$pos_close = strpos($content, $str_close);
+		if ($pos_open !== false && $pos_close !== false) { //existe el trozo de texto que buscas?
+			//$subcontent = substr($content, $pos_open, ($pos_close - $pos_open)); 
+			
+			$subcontent = substr($content, ($pos_open+strlen($str_open)), ($pos_close - $pos_open)); 
+			echo($subcontent);//subcontent: los datos que necesitas
+			echo '<pre>';
+			$rut = substr($subcontent, 0, strpos($subcontent, "-")+2);
+			echo '<pre>';
+			$subcontent2 = substr($subcontent, (1+strpos($subcontent, "-")+1), ($pos_close - strpos($subcontent, "-")+1));
+			echo ($subcontent2);
+			echo '<pre>';
+			$datolimpio = [
+				"rut" => $rut,
+				"nombre" => "calmao",
+				"mail" => "xxx@ss.cl",
+				"fono" => "9999999"
+			];
+
+			echo json_encode($datolimpio);
+		}
+		echo ("---------------------");
 		//$casi = strip_tags($result["inbox"][$i]["message"]);
-		//var_dump(explode(" ", $casi));
+		//var_dump(explode(" ", $casi));	
 	}
 }
 // var_dump($result);
 
-$array_num = count($result);
+//$array_num = count($result);
 // echo($array_num);
 
 // for ($i = 0; $i < $array_num; ++$i){
